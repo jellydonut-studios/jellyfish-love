e14bcac3be137372f8ab0cbbead48767879c4152
Jellyfish Nguyen <gastlyarts32@gmail.com>
2026-02-20 13:44:26 -0500

    changed name again
    images are missing now

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jellyfish Love Lore",
+    pageTitle: "Quartz 4.0",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
