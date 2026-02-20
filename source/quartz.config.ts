6c9ae7fc6876117724070804cfd0fe271985801b
Jellyfish Nguyen <gastlyarts32@gmail.com>
2026-02-19 21:48:17 -0500

    changed name back
    it broke the site </3

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jellyfish Love! Site",
+    pageTitle: "🪴 Quartz 4.0",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
