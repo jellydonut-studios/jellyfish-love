d75ae3f533062d15d5783cce2294c38ac26a9265
Jellyfish Nguyen <gastlyarts32@gmail.com>
2026-02-20 13:37:37 -0500

    Update quartz.config.ts
    page wouldn't load with the previous name

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jellyfish Love! Lore",
+    pageTitle: "Jellyfish Love Lore",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
