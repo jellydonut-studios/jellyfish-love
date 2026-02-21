9843a15067704750014fc7280307c57765f0067c
Jellyfish Nguyen <gastlyarts32@gmail.com>
2026-02-21 08:44:08 -0500

    page title changed
    testing out changing the page title

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4.0",
+    pageTitle: "Q",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
