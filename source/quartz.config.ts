f0f79a41cd244dcdd84841b69ee79674cb5584a3
Jellyfish Nguyen <gastlyarts32@gmail.com>
2026-02-21 08:18:34 -0500

    minor fixes
    did work on the canvas and changed some stuff in the config

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jellydonut-studios.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
