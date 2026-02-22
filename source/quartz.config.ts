e1c69889b917413e2bc3583f34cc9eb0da342765
Jellyfish Nguyen <gastlyarts32@gmail.com>
2026-02-22 18:01:07 -0500

    Obsidian Edits
    edited page title, quartz config, and added more to the brainstorming canvas

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jellyfish Love Lore",
+    pageTitle: "QJellyfish Love Lore",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
